31bb889ee93c8e0eff52f0027913fbe82454d6f1
Chetan Borade <chetanborade9@gmail.com>
2025-04-08 11:25:21 +0530

    added image tag and removed latest

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (_, res) => {
-  res.send('<h1>🚀 Hello fron mKS via GitHub Actions!</h1>');
+  res.send('<h1>🚀 Hello fron mKS via Actions!</h1>');
 });
 
 app.listen(port, () => {
