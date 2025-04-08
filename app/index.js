38c5d58339433fdb79a666fffa5610a0f6c174aa
Chetan Borade <chetanborade9@gmail.com>
2025-04-08 12:31:04 +0530

    rechecking

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (_, res) => {
-  res.send('<h1>🚀 Hello fron mKS via Actions!</h1>');
+  res.send('<h1>🚀 H Hey ello fron mKS via Actions!</h1>');
 });
 
 app.listen(port, () => {
